43e1667821d19d25caf46b0e17ab886be5a46537
Thijs Mergaert <thijs.mergaert@gmail.com>
2021-08-06 23:39:25 -0700

    Finish basic implementation

--- index.js
+++ index.js
@@ -1,17 +1,90 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, MessageEmbed, MessageSelectMenu, MessageSelectOptionData, MessageActionRow } = require('discord.js');
+const { Gallery } = require('./lib/gallery');
+const TurndownService = require('turndown');
 
 // create a new Discord client
 const client = new Client({ intents: [ Intents.FLAGS.GUILDS,  Intents.FLAGS.GUILD_MESSAGES ] });
+const regex = /\[\[(.*)\]\]/;
+const turndownService = new TurndownService({hr: ''});
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
-client.once('ready', () => {
-	console.log('Ready!');
+client.once('ready', async () => {
+    this.gallery = new Gallery();
+    await this.gallery.parseGallery("./Hero Realms Card Gallery - Images - Card Gallery.csv");
+    console.log('Ready!');
 });
 
-client.on('messageCreate', message => {
-	console.log(message);
+client.on('messageCreate', async message => {
+    const matches = message.content.match(regex);
+        if (matches) {
+            try {
+                const results = await this.gallery.searchGallery(matches[1]);
+                if (results.length === 0) {
+                    message.reply('No cards found with this name');
+                    return;
+                }
+                if (results.length > 1) {
+                    message.reply({content: 'Multiple matches found, please select your choice:', components: [await generateSelectMenu(results)]});
+                    return;
+                }
+                message.reply({content: `Card found`, embeds: await generateCardEmbeds(this.gallery, results[0].index)});
+            } catch (e) {
+                console.log(e);
+            }
+        };
 });
+
+client.on('interactionCreate', async interaction => {
+    try {
+        const message = interaction.message;
+        if (interaction.customId === 'selectMenu') {
+            const cardIndex = Number(interaction.values[0]);
+            interaction.update({content: 'Card selected', embeds: await generateCardEmbeds(this.gallery, cardIndex), components: []});
+        }
+    } catch (e) {
+        console.log(e);
+        interaction.update({content: 'Something went wrong, please try again later.', components: []});
+    }
+});
+
+async function generateSelectMenu(cards) {
+    const selectOptions = cards.map(card => {
+        const entry = card.entry;
+        const selectOption = {
+            label: entry.Name,
+            value: String(card.index),
+            description: "".concat(entry.Type ? `Type: ${entry.Type}, ` : "", entry.Faction ? `Faction: ${entry.Faction}, ` : "",  `Set: ${entry['Set Name']}`).slice(0, 100)
+        };
+        return selectOption;
+    })
+    const selectMenu = new MessageSelectMenu()
+        .addOptions(selectOptions)
+        .setCustomId('selectMenu');
+    const actionRow = new MessageActionRow()
+        .addComponents(selectMenu);
+    return actionRow;
+}
+
+async function generateCardEmbeds(gallery, index) {
+    const card = await gallery.getGalleryItem(index);
+    const cardImageUrls = card['Image URLs'].split(' | ');
+    const embeds = [];
+    const name = card.Name;
+    for (const index in cardImageUrls) {
+        const cardEmbed = new MessageEmbed()
+            .setColor('#b04343')
+            .setTitle(card.Name)
+            .setFields(
+                { name: 'Type', value: card.Type }
+            )
+            .setURL('https://www.herorealms.com/card-gallery/')
+            .setImage(cardImageUrls[index])
+            .setFooter(`Set: ${card['Set Name']}        Artist: ${card.Artist}`);
+        embeds.push(cardEmbed);
+    }
+    return embeds;
+}
 
 // login to Discord with your app's token
 client.login(process.env.DISCORD_TOKEN).catch(console.log);
